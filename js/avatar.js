1568fc5a55e0c592d468cf266f15d0f462e334c9
Safe_Mode <safe2mode@gmail.com>
2018-05-05 16:59:42 +0300

    Добавляет возможность загрузки фото игрока перетаскиванием

--- js/avatar.js
+++ js/avatar.js
@@ -2,12 +2,15 @@
 
 (function () {
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var HOVER_COLOR = '#da641a';
 
   var fileChooserEl = document.querySelector('input[type=file]');
+  var fileChooserBtn = document.querySelector('.setup-user-upload');
   var previewEl = document.querySelector('.setup-user-pic');
+  var initColor = window.getComputedStyle(fileChooserBtn).color;
 
-  fileChooserEl.addEventListener('change', function () {
-    var file = fileChooserEl.files[0];
+  var setUserFile = function (file) {
+    file = (!file) ? fileChooserEl.files[0] : file;
     var fileName = file.name.toLowerCase();
 
     var matches = FILE_TYPES.some(function (it) {
@@ -23,5 +26,31 @@
 
       reader.readAsDataURL(file);
     }
+  };
+
+  fileChooserEl.addEventListener('change', function () {
+    setUserFile();
+  });
+
+  document.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+  });
+
+  document.addEventListener('drop', function (evt) {
+    evt.preventDefault();
+  });
+
+  fileChooserBtn.addEventListener('dragenter', function (evt) {
+    evt.currentTarget.style.color = HOVER_COLOR;
+  });
+
+  fileChooserBtn.addEventListener('dragleave', function (evt) {
+    evt.currentTarget.style.color = initColor;
+  });
+
+  fileChooserBtn.addEventListener('drop', function (evt) {
+    evt.preventDefault();
+    var file = evt.dataTransfer.files[0];
+    setUserFile(file);
   });
 })();
