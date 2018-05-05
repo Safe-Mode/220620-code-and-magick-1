e71706d38c09ddbacd1fe58a1ae5f8a77f8410f4
Safe_Mode <safe2mode@gmail.com>
2018-05-05 15:44:28 +0300

    Меняет аватар пользователя

--- js/avatar.js
+++ js/avatar.js
@@ -1,5 +1,27 @@
 'use strict';
 
 (function () {
-  
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var fileChooserEl = document.querySelector('input[type=file]');
+  var previewEl = document.querySelector('.setup-user-pic');
+
+  fileChooserEl.addEventListener('change', function () {
+    var file = fileChooserEl.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        previewEl.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
 })();
